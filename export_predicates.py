c805513b7c99cb5e6dfae880f27067e74a4296d7
William Edmisten <wcedmisten@gmail.com>
2024-02-25 13:41:16 -0500

    Add predicate CSV export script

---
+++ export_predicates.py
@@ -0,0 +1,18 @@
+import json
+
+import sqlite3
+
+con = sqlite3.connect("devices.db")
+cur = con.cursor()
+
+res = cur.execute("SELECT * FROM predicate_graph_edge")
+edges = res.fetchall()
+
+with open("predicates.csv", "w") as f:
+    f.write("device,predicate\n")
+
+    for edge in edges:
+        # node_from,node_to
+        node_from, node_to = edge
+
+        f.write(node_to + "," + node_from + "\n")
