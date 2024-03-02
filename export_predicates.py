037e932f73359af615a1b81670d2dec690dc8ebc
William Edmisten <wcedmisten@gmail.com>
2024-03-02 12:24:12 -0500

    Order predicates

--- export_predicates.py
+++ export_predicates.py
@@ -5,7 +5,7 @@ import sqlite3
 con = sqlite3.connect("devices.db")
 cur = con.cursor()
 
-res = cur.execute("SELECT * FROM predicate_graph_edge")
+res = cur.execute("SELECT * FROM predicate_graph_edge ORDER BY node_to DESC")
 edges = res.fetchall()
 
 with open("predicates.csv", "w") as f:
