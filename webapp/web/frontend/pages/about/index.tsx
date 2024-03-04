02f1d719c8d137d00c64f8e202274a0b18046c6a
William Edmisten <wcedmisten@gmail.com>
2024-03-03 22:54:43 -0500

    Update about page

--- webapp/web/frontend/pages/about/index.tsx
+++ webapp/web/frontend/pages/about/index.tsx
@@ -9,18 +9,29 @@ export const LandingPage = () => {
             <Row>
                 <Col>
                     <h1 className={style.subTitle}>About 510k.fyi</h1>
+
+                    <img className={style.avatar} src="/images/william.jpg"></img>
+
                     <p className={style.text}>
-                        510k.fyi is an open source website developed by <a href="https://wcedmisten.fyi/about/">William Edmisten</a>.
-                        The website is meant to help consumers, device manufacturers, and the FDA better understand the predicate device ancestry
-                        of cleared devices.
+                        Hello! My name is William Edmisten, and I'm the software engineer who built 510k.fyi.
+                        You can read more about me on my <a href="https://wcedmisten.fyi/about/">blog</a>.
+                    </p>
+                    
+                    <p className={style.text}>    
+                        I built 510k.fyi to help consumers, device manufacturers, and the FDA better understand the predicate device ancestry
+                        of devices cleared through the 510k process.
+
+                    </p>
+                    <p className={style.text}>
                         As of February 2024, the FDA's 510k database does not directly show the predicates used for each device.
-                        The only way to find the predicates for a 510k is to manually examine a PDF summary of the 510k application.
+                        The only way to find the predicates for  510k is to manually examine the PDF summary of each 510k application.
                         This makes any systematic review of predicate device ancestry difficult.</p>
 
                     <p className={style.text}>
-                        To make systematic review more feasible, I collected all 
-                        of the available 510k PDF summaries and used OCR to find the predicates mentioned for each application.
-                        I collected this data into the database which powers this website.
+                        I collected all of the available 510k PDF summaries and used OCR to
+                        find the predicates mentioned for each application, in order to simplify 
+                        the data, and help show the relationship between recalled devices and 
+                        currently marketed ones.
                     </p>
                     
                     <p className={style.text}>
