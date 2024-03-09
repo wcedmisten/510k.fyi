d68fe994b1d05fac9fd784aa6ebb7c12c160ca64
William Edmisten <wcedmisten@gmail.com>
2024-03-09 14:12:06 -0500

    Add more analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
+import seaborn as sns
 import networkx as nx
 import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import statistics
 
 con = sqlite3.connect("../scripts/devices.db")
 cur = con.cursor()
@@ -8,13 +12,34 @@ cur = con.cursor()
 cur.execute("SELECT node_from, node_to FROM predicate_graph_edge;")
 all_edges = cur.fetchall()
 
-# detect a cycle
 g = nx.DiGraph(all_edges)
 
-degrees = [g.degree(n) for n in g.nodes()]
-plt.hist(degrees)
-# plt.show()
+# avg degree
+print("Average degree:")
+print(sum(map(lambda x: x[1], g.in_degree())) / len(g))
 
-print("Calculating longest path...")
+# longest path
+
+print("Calculating longest path:")
 longest_dag = nx.dag_longest_path(g)
 print(len(longest_dag))
+
+# median 
+print("Median degree")
+print(statistics.median(map(lambda x: x[1], g.in_degree())))
+
+# degree histogram
+
+degrees = [g.in_degree(n) for n in g.nodes()]
+
+data = pd.DataFrame(degrees)
+
+fig, ax1 = plt.subplots()
+
+hist = sns.histplot(data=data, log_scale=True, legend=None)
+
+ax1.set_title("Histogram of Node Degrees", fontsize=18)
+ax1.set_xlabel("Degree", fontsize=18)
+ax1.set_ylabel("Count", fontsize=18)
+
+plt.show()
